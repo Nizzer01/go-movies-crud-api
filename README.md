a66a2b01e34df70a611cf8b3eea281aceb6c56cd
Gav Shields <65550958+Nizzer01@users.noreply.github.com>
2024-07-03 16:52:29 +0100

    Initial setup of struts server setup BP code for methods & routs

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # go-movies-crud-api
 Crud Api for movies
+
+- Uses gorilla/mux v1.8.1
+- Uses struts etc instead of database 
---
+++ main.go
@@ -0,0 +1,45 @@
+package go_movies_crud_api
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	//Preload some data
+	movies = append(movies, Movie{
+		ID:    "1",
+		Isbn:  "438227",
+		Title: "Movie 1",
+		Director: &Director{
+			FirstName: "John",
+			LastName:  "Smith"},
+	})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server on port 8000\n")
+	log.Fatal(http.ListenAndServe(":8000", r))
+}
